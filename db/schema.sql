9eecdcb2dcd0dc93b7b3c1762977f4cae3444afb
Marina <marinajalonzo1@gmail.com>
2024-03-08 04:30:14 -0600

    Basic file and table setup

--- db/schema.sql
+++ db/schema.sql
@@ -4,21 +4,21 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
-    department_name VARCHAR(30)
+    id: INT PRIMARY KEY,
+    name: VARCHAR(30)
 );
 
-CREATE TABLE roles (
-    id INT NOT NULL,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT NOT NULL
+CREATE TABLE role (
+    id: INT PRIMARY KEY,
+    title: VARCHAR(30),
+    salary: DECIMAL,
+    department_id: INT
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT
+    id: INT PRIMARY KEY,
+    first_name: VARCHAR(30),
+    last_name: VARCHAR(30),
+    role_id: INT,
+    manager_id: INT
 );
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,10 +1,10 @@
-INSERT INTO department (id department_name)
+INSERT INTO department (id, name)
 VALUES (2, 'Engineering'),
         (3, 'Finance'),
         (4, 'Legal'),
         (1, 'sales');
 
-INSERT INTO roles (id title department_id salary)
+INSERT INTO roles (id, title, department_id, salary)
 VALUES (1, 'Sales Lead', 'Sales', 1000000),
         (2, 'Salesperson', 'Sales', 800000),
         (3, 'Lead Engineer', 'Engineering', 150000),
@@ -14,7 +14,7 @@ VALUES (1, 'Sales Lead', 'Sales', 1000000),
         (7, 'Legal Team Lead', 'Legal', 250000),
         (8, 'Lawyer', 'Legal', 190000);
 
-INSERT INTO employee (id first_name last_name title department_name salary manager_id)
+INSERT INTO employee (id, first_name, last_name, title, department_name, salary, manager_id)
 VALUES (1, 'John', 'Doe', 'Sales Lead', 'Sales', 100000),
         (2 'Mike', 'Chan', 'Salesperson', 'Sales', 80000),
         (3, 'Ashley', 'Rodriguez', 'Lead Engineer', 'Engineering', 150000),
