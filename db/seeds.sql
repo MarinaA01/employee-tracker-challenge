e6436c660147b82a8d1dac9c4bc21b572546339e
Marina <marinajalonzo1@gmail.com>
2024-03-12 18:19:01 -0500

    SQL Query and Inquirer setup.

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,7 +4,7 @@ VALUES (2, 'Engineering'),
         (4, 'Legal'),
         (1, 'sales');
 
-INSERT INTO roles (id, title, department_id, salary)
+INSERT INTO role (id, title, department_id, salary)
 VALUES (1, 'Sales Lead', 'Sales', 1000000),
         (2, 'Salesperson', 'Sales', 800000),
         (3, 'Lead Engineer', 'Engineering', 150000),
@@ -14,12 +14,12 @@ VALUES (1, 'Sales Lead', 'Sales', 1000000),
         (7, 'Legal Team Lead', 'Legal', 250000),
         (8, 'Lawyer', 'Legal', 190000);
 
-INSERT INTO employee (id, first_name, last_name, title, department_name, salary, manager_id)
-VALUES (1, 'John', 'Doe', 'Sales Lead', 'Sales', 100000),
-        (2 'Mike', 'Chan', 'Salesperson', 'Sales', 80000),
-        (3, 'Ashley', 'Rodriguez', 'Lead Engineer', 'Engineering', 150000),
-        (4, 'Kevin', 'Tupik', 'Software Engineer', 'Engineering', 120000),
-        (5, 'Kunal', 'Singh', 'Account Manager', 'Finance', 160000),
-        (6, 'Malia', 'Brown', 'Accountant', 'Finance', 125000),
-        (7, 'Sarah', 'Lourd', 'Legal Team Lead', 'Legal', 250000),
-        (8, 'Tom', 'Allen', 'Lawyer', 'Legal', 190000);
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, 'John', 'Doe', 'Sales Lead', 'Sales', 100000, null),
+        (2 'Mike', 'Chan', 'Salesperson', 'Sales', 80000, John Doe),
+        (3, 'Ashley', 'Rodriguez', 'Lead Engineer', 'Engineering', 150000, null),
+        (4, 'Kevin', 'Tupik', 'Software Engineer', 'Engineering', 120000, Ashley Rodriguez),
+        (5, 'Kunal', 'Singh', 'Account Manager', 'Finance', 160000, null),
+        (6, 'Malia', 'Brown', 'Accountant', 'Finance', 125000, Kunal Singh),
+        (7, 'Sarah', 'Lourd', 'Legal Team Lead', 'Legal', 250000, null),
+        (8, 'Tom', 'Allen', 'Lawyer', 'Legal', 190000, Sarah Lourd);
--- server.js
+++ server.js
@@ -1,10 +1,76 @@
 const express = require("express");
+const inquirer = require("inquirer");
 
 const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
-
 const app = express();
 
 app.use(express.urlencoded({extended: false}));
+app.use(express.json());
 
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        user: "root",
+        password: "Oreo0912!",
+        database: "company_db"
+    },
+    console.log("Connected to the company_db database.")
+);
+
+const viewAllEmployees = () => {
+    db.query("SELECT * FROM employee", (err, rows) => {
+        console.table(rows);
+    });
+}
+
+const viewAllDepartments = () => {
+    db.query("SELECT * FROM department", (err, rows) => {
+        console.table(rows);
+    });
+}
+
+const viewAllRoles = () => {
+    db.query("SELECT * FROM role", (err, rows) => {
+        console.table(rows);
+    });
+}
+
+// https://javascript.plainenglish.io/how-to-inquirer-js-c10a4e05ef1f
+
+return inquirer
+    .prompt([
+        {
+            type: "list",
+            name: "choices",
+            message: "What would you like to do?",
+            choices: [
+                "View All Employees",
+                "View All Departments",
+                "View All Roles",
+                "Add Employee",
+                "Add Department",
+                "Add Role",
+                "Update Employee Role",
+                "Quit"
+            ]
+        }
+    ])
+    .then((answers) => {
+        if(answers.choices === "View All Employees") {
+            viewAllEmployees();
+        } else if(answers.choices === "View All Departments") {
+            viewAllDepartments();
+        } else if(answers.choices === "View All Roles") {
+            viewAllRoles();
+        }
+    });
+
+app.use((req, res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
