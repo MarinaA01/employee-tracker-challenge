4f5ec71caafd2c76548c6b232f90795a7faea8cb
Marina <marinajalonzo1@gmail.com>
2024-03-12 18:56:33 -0500

    Seeds fixes.

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,24 +2,24 @@ INSERT INTO department (id, name)
 VALUES (2, 'Engineering'),
         (3, 'Finance'),
         (4, 'Legal'),
-        (1, 'sales');
+        (1, 'Sales');
 
 INSERT INTO role (id, title, department_id, salary)
-VALUES (1, 'Sales Lead', 'Sales', 1000000),
-        (2, 'Salesperson', 'Sales', 800000),
-        (3, 'Lead Engineer', 'Engineering', 150000),
-        (4, 'Software Engineer', 'Engineering', 120000),
-        (5, 'Account Manager', 'Finance', 160000),
-        (6, 'Accountant', 'Finance', 125000),
-        (7, 'Legal Team Lead', 'Legal', 250000),
-        (8, 'Lawyer', 'Legal', 190000);
+VALUES (1, 'Sales Lead', 1, 1000000),
+        (2, 'Salesperson', 1, 800000),
+        (3, 'Lead Engineer', 2, 150000),
+        (4, 'Software Engineer', 2, 120000),
+        (5, 'Account Manager', 3, 160000),
+        (6, 'Accountant', 3, 125000),
+        (7, 'Legal Team Lead', 4, 250000),
+        (8, 'Lawyer', 4, 190000);
 
 INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'John', 'Doe', 'Sales Lead', 'Sales', 100000, null),
-        (2 'Mike', 'Chan', 'Salesperson', 'Sales', 80000, John Doe),
-        (3, 'Ashley', 'Rodriguez', 'Lead Engineer', 'Engineering', 150000, null),
-        (4, 'Kevin', 'Tupik', 'Software Engineer', 'Engineering', 120000, Ashley Rodriguez),
-        (5, 'Kunal', 'Singh', 'Account Manager', 'Finance', 160000, null),
-        (6, 'Malia', 'Brown', 'Accountant', 'Finance', 125000, Kunal Singh),
-        (7, 'Sarah', 'Lourd', 'Legal Team Lead', 'Legal', 250000, null),
-        (8, 'Tom', 'Allen', 'Lawyer', 'Legal', 190000, Sarah Lourd);
+VALUES (1, 'John', 'Doe', 1, null),
+        (2 'Mike', 'Chan', 2, 1),
+        (3, 'Ashley', 'Rodriguez', 3, null),
+        (4, 'Kevin', 'Tupik', 4, 3),
+        (5, 'Kunal', 'Singh', 5, null),
+        (6, 'Malia', 'Brown', 6, 5),
+        (7, 'Sarah', 'Lourd', 7, null),
+        (8, 'Tom', 'Allen', 8, 7);
